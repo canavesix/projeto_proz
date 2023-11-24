df5c3d621b7604209e68863e74104ffadb434093
Gustavo Canavesi <gustavo.canavesi@outlook.com>
2023-11-24 09:26:15 -0300

    subindo a pasta com css e js da tela de catalogo

---
+++ catalogo/catalogo.js
@@ -0,0 +1,39 @@
+let quantidadeSubtotal = document.getElementById("quantidade-subtotal");
+let valorSubtotal = document.getElementById("valor-subtotal");
+
+let subtotalInfo = {
+  quantidade: 1,
+  valorUnitario: 11.66,
+};
+
+function atualizandoSubtotal() {
+  quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
+  valorSubtotal.innerText = (subtotalInfo.valorUnitario * subtotalInfo.quantidade).toFixed(2);
+}
+
+atualizandoSubtotal();
+
+//Variaveis/Dados
+let botaoAdicionar = document.querySelector("#btn-adicionar-produto-01");
+let quantidadedeProduto = document.querySelector("#quantidade-produto-01");
+let botaoDiminuir = document.querySelector("#btn-subtrair-produto-01");
+
+//funcoes
+function adicionarUm() {
+  
+  quantidadedeProduto.value = Number(quantidadedeProduto.value) + 1;
+
+  
+  subtotalInfo.quantidade = Number(quantidadedeProduto.value);
+  atualizandoSubtotal();
+}
+
+function diminuirUm() {
+  
+  if (quantidadedeProduto.value > 1) {
+    quantidadedeProduto.value = Number(quantidadedeProduto.value) - 1;
+    
+    subtotalInfo.quantidade = Number(quantidadedeProduto.value);
+    atualizandoSubtotal();
+  }
+}
