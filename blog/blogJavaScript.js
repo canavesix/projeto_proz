3f9f5753623b1026b3e3a95dcc982156bae003f6
Fabio <fabio_rocka@hotmail.com>
2023-12-04 16:31:35 -0300

    Campo de pesquisa melhorado. Código JS na pesquisa melhorado. Inserido botão de pesquisa. Função Enter para pesquisar adicionada. Alterações na estilização da página, para padronização com as demais. Novas Tags no HTML da página Blog.

--- blog/blogJavaScript.js
+++ blog/blogJavaScript.js
@@ -32,10 +32,69 @@ window.nextSlide = nextSlide;
 
 // FUNCTION - REALIZAR PESQUISA NA PÁGINA
 
+/*
 function realizarPesquisa() {
   // Obtenha o valor digitado na caixa de pesquisa
   var termoPesquisa = document.getElementById("searchBox").value;
 
   // Execute a lógica da pesquisa aqui (pode ser exibindo em um console por enquanto)
   console.log("Termo de pesquisa: " + termoPesquisa);
+}
+*/
+
+
+// SEARCH DA PÁGINA - TESTE 2
+
+/* function searchPage() {
+  // Obter o valor de pesquisa do input
+  var searchTerm = document.getElementById('searchBox').value;
+
+  // Verificar se o termo de pesquisa está presente na página
+  var pageContent = document.body.innerText;
+  var searchTermIndex = pageContent.indexOf(searchTerm);
+
+  if (searchTermIndex !== -1) {
+      alert('Termo de pesquisa encontrado na posição: ' + searchTermIndex);
+  } else {
+      alert('Termo de pesquisa não encontrado na página.');
+  }
+}
+*/
+
+// SEARCH DA PÁGINA - TESTE 3
+
+function searchPage() {
+  // Obter o valor de pesquisa do input
+  var searchTerm = document.getElementById('searchBox').value.toLowerCase();
+
+  // Remover destaque de pesquisas anteriores
+  var highlightedRows = document.querySelectorAll('.highlight');
+  highlightedRows.forEach(function(row) {
+      row.classList.remove('highlight');
+  });
+
+  // Procurar nos elementos das linhas da tabela
+  var rows = document.querySelectorAll('tr[id^="Artigo"]');
+  var found = false;
+
+  rows.forEach(function(row) {
+      var rowText = row.innerText.toLowerCase();
+
+      if (rowText.includes(searchTerm)) {
+          found = true;
+          row.classList.add('highlight');
+          alert('Termo de pesquisa encontrado na linha da tabela com ID: ' + row.id);
+      }
+  });
+
+  if (!found) {
+      alert('Termo de pesquisa não encontrado nas linhas da tabela.');
+  }
+}
+
+  // Apertar enter para efetuar pesquisa
+function handleKeyPress(event) {
+  if (event.key === 'Enter') {
+      searchPage();
+  }
 }
